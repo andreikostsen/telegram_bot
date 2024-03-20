d6970fb42e87070d5d33cfd32cd7cb8daa84010f
andreikostsen <pks@tut.by>
2024-03-20 17:24:23 +0300

    feat: access and format section added

--- index.js
+++ index.js
@@ -1,38 +1,95 @@
-const { Telegraf, Markup } = require('telegraf')
-const { message } = require('telegraf/filters')
+const {Telegraf, Markup} = require('telegraf')
+const {message} = require('telegraf/filters')
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const present = `
+const gift = `
 <a href="https://youtu.be/VJz4NZgc3Fo?si=Io1PylmOGYUDi1ox">Видео практика “Шея и декольте“</a>
 `
-let trigger = false
 
-
-console.log(trigger)
-
-bot.start(  async (ctx) => {
-    await ctx.replyWithPhoto({ source: './img/Katya_invitation1.png'})
+bot.start(async (ctx) => {
+    await ctx.replyWithPhoto({source: './img/Katya_invitation1.png'})
 
     await ctx.replyWithHTML(`Привет, Это <b>Екатерина Гарницкая</b> и я очень рада нашей встрече. ` +
         `Я подготовила для тебя подарок – Видеоурок <b>«Самомассаж шеи и декольте скребком гуаша»</b>❤️❤️`)
 
-    await ctx.replyWithPhoto({ source: './img/Katya_invitation2.jpg'})
+    await ctx.replyWithPhoto({source: './img/Katya_invitation2.jpg'})
 
     await ctx.replyWithHTML(`Я – Екатерина Гарницкая, создатель большого проекта Human Woman о ` +
-     `женской красоте и здоровье и автор нескольких марафонов: “PRO Грудь”, “Ретрита Гуаша”\n\n` +
-     `· сертифицированный специалист по неинвазивным методам омоложения (Гуаша, осетоэстетика, трансбуккалый, миофасциальный и лимфодренажный массажи, асахи, faceyoga, тэйпирование)\n` +
-     `· практикующий остеопат\n` +
-     `· специалист по послеродовому восстановлению и телесно-эмоциональному освобождению\n` + 
-     `· эмбодимент практик\n`
+        `женской красоте и здоровье и автор нескольких марафонов: “PRO Грудь”, “Ретрита Гуаша”\n\n` +
+        `· сертифицированный специалист по неинвазивным методам омоложения (Гуаша, осетоэстетика, трансбуккалый, миофасциальный и лимфодренажный массажи, асахи, faceyoga, тэйпирование)\n` +
+        `· практикующий остеопат\n` +
+        `· специалист по послеродовому восстановлению и телесно-эмоциональному освобождению\n` +
+        `· эмбодимент практик\n`
     )
 
     await ctx.replyWithHTML('<b>Нажми на кнопку чтобы получить подарок</b>', Markup.inlineKeyboard(
         [
-          [Markup.button.callback('Получить подарок!', 'present')],
+            [Markup.button.callback('Получить подарок!', 'gift')],
         ]
-      ))
+    ))
+
+    // await ctx.replyWithHTML('Выбирай',
+    //     getMainMenu()
+    // )
+
+})
+
+bot.action(['yes', 'no'], ctx => {
+    if (ctx.callbackQuery.data === 'yes') {
+        ctx.editMessageText('Ваша задача успешно добавлена')
+    } else {
+        ctx.deleteMessage()
+    }
+})
+
+
+bot.action(['feedback', 'access'], async ctx => {
+
+//ПРОГРАММА КУРСА И ОТЗЫВЫ
+
+    if (ctx.callbackQuery.data === 'feedback') {
+
+        await ctx.replyWithPhoto({source: './img/feedback/feedback1.jpg'})
+        await ctx.replyWithPhoto({source: './img/feedback/feedback2.jpg'})
+        await ctx.replyWithPhoto({source: './img/feedback/feedback3.jpg'})
+        await ctx.replyWithPhoto({source: './img/feedback/feedback4.jpg'})
+        await ctx.replyWithPhoto({source: './img/feedback/feedback5.jpg'})
+        await ctx.replyWithPhoto({source: './img/feedback/feedback6.jpg'})
+
+        await ctx.replyWithHTML(`<b>ПРОГРАММА КУРСА:</b> \n` +
+            `➝ Вводная лекция по анатомии лица\n` +
+            `➝ 7 видео-практик со скребком Гуаша\n` +
+            `➝ Общий комплекс на каждый день с объединением всех техник для эффективного результата\n` +
+            `➝ Лимфодренажный блок\n` +
+            `➝ Техника SMAS лифтинга при помощи Гуаша\n` +
+            `➝ Энергетическое расслабление лица\n` +
+            `➝ Бьюти-медитация\n` +
+            `➝ Работа с дыханием и диафрагмой\n` +
+            `➝ Остеопатическая практика: работа с овалом лица\n`)
+
+
+        setTimeout(async () => {
+            await ctx.replyWithHTML(`<b>Как тебе?</b> \n` +
+                `Если ты еще не сделала практику <b>«Декольте-шея-ключицы»</b>, то можешь приступить к ней прямо сейчас🔥🔥🔥\n` +
+                `Переходи по <a href='https://youtu.be/VJz4NZgc3Fo'>ссылке</a>`
+            )
+        }, 5000)
+    }
+
+    //ФОРМАТ И ДОСТУП
+
+        if (ctx.callbackQuery.data === 'access') {
+
+            console.log("access")
+
+            await ctx.replyWithPhoto({source: './img/access.jpg'},
+                {caption: "Курс в записи, будет доступен сразу после оплаты. Все материалы структурно размещены в закрытом Telegram-канале.\n\nДоступ к курсу - 6 месяцев"})
+
+        }
+
+
 })
 
 
@@ -50,19 +107,19 @@ function addActionBot(id_btn, src_img, text, preview) {
       try {  
 
 
-          if (id_btn == 'present') {
+          if (id_btn == 'gift') {
 
               // Set a timer for 5 seconds
               setTimeout(async () => {
-                  await ctx.replyWithHTML(`<b>Как тебе практика?</b>\n\n` +
-                      `Хочешь научиться большему и владеть волшебным инструментом Гуаша, делать самомассаж на уровне профессионала?\n\n` +
+                  await ctx.replyWithHTML(` <b> Как тебе практика ? </b>\n\n` +
+                      `Хочешь научиться большему и владеть волшебным инструментом Гуаша, делать самомассаж на уровне профессионала ?\n\n` +
                       `Тогда нажимай кнопку ниже, познакомиться с курсом, который я для тебя подготовила`)
 
                   await ctx.replyWithHTML('<b>Меню:</b>', Markup.inlineKeyboard(
                       [
-                          [Markup.button.callback('Тарифы и стоимость', 'btn_1')],
-                          [Markup.button.callback('Программа курса и отзывы', 'btn_2')],
-                          [Markup.button.callback('Записаться на курс', 'btn_3')],
+                          [Markup.button.callback('Программа курса и отзывы', 'feedback')],
+                          [Markup.button.callback('Формат и доступ', 'access')],
+                          [Markup.button.callback('Тарифы и стоимость', 'prices')],
                       ]
                   ))
 
@@ -74,7 +131,6 @@ function addActionBot(id_btn, src_img, text, preview) {
         await ctx.answerCbQuery()
         if (src_img !== false) {
 
-            trigger = true;
           await ctx.replyWithPhoto({
             source: src_img
           })
@@ -88,13 +144,19 @@ function addActionBot(id_btn, src_img, text, preview) {
     })
   }
   // Обработчик кнопок с помощью функции
-   addActionBot('present', false, present, false)
-   addActionBot('btn_2', false, "tyjtyj", true)
-  addActionBot('btn_3', false, 'gghet', false)
+addActionBot('gift', false, gift, false)
+addActionBot('feedback', './img/feedback/feedback1.jpg', '', true)
+addActionBot('btn_3', false, 'gghet', false)
 
 
-
-
+function getMainMenu() {
+    return Markup.inlineKeyboard(
+        [
+            [Markup.button.callback('Да', 'yes')],
+            [Markup.button.callback('Нет', 'no')],
+        ]
+    )
+}
 
 
 
